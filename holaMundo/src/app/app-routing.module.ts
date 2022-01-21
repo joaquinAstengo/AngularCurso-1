b7d1179aea12f1862b749b834bf2bc2f15ea3a14
Joaquin Notebook <joaquin.astengo@gmail.com>
2022-01-21 20:21:34 -0300

    21-1-22

--- holaMundo/src/app/app-routing.module.ts
+++ holaMundo/src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
 
 const routes: Routes = [
-  {path: '',
-    loadChildren: ()=>import('@modules/home/home.module').then(m => m.HomeModule)}
+  {
+    path: '',
+    loadChildren: ()=> import('@modules/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'tracaks',
+    loadChildren: ()=> import('@modules/tracks/tracks.module').then(m => m.TracksModule)
+  },
 ];
 
 @NgModule({
--- holaMundo/src/app/modules/history/history.module.ts
+++ holaMundo/src/app/modules/history/history.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HistoryRoutingModule } from './history-routing.module';
+import { HistoryPageComponent } from './history-page/history-page.component';
+import { HistoryPagesComponent } from './pages/history-pages/history-pages.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HistoryPageComponent,
+    HistoryPagesComponent
+  ],
   imports: [
     CommonModule,
     HistoryRoutingModule
